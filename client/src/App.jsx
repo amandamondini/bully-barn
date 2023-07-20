564d8ec75a9b10b729bfa66da48300fef05a0881
Collin Letourneau <collin.r.letourneau@gmail.com>
2023-07-20 15:30:33 -0400

    Finished Application FrontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/Auth/Auth";
-import Dog from './components/Dog/Dog'
+import Dog from "./components/Dog/Dog";
 import Nav from "./components/Nav/Nav";
+import Form from "./components/Form/Form";
+import AdminDash from "./components/Admin-Dash/Admin-Dash";
 
 const renderNav = (Component) => {
-  return (
-    <>
-    <Nav />
-    <Component />
-    </>
-  )
-}
+    return (
+        <>
+            {/* <Nav /> */}
+            {/* <Component /> */}
+            <AdminDash />
+        </>
+    );
+};
 
 function App() {
     return (
-      <Router>
-    <Routes>
-      <Route path='/' element={renderNav(Dog)}/>
-      <Route path="/auth" element={<Auth />} />
-      </Routes>
-  </Router>
-  
+        <Router>
+            <Routes>
+                <Route path="/" element={renderNav(Dog)} />
+                <Route path="/auth" element={<Auth />} />
+            </Routes>
+        </Router>
     );
 }
 
